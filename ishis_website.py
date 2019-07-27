cba9d151f41ab4e7ce6c67136f7d39aba5027586
thelisteningrock <47855437+thelisteningrock@users.noreply.github.com>
2019-07-27 19:22:26 +0100

    Update ishis_website.py

--- ishis_website.py
+++ ishis_website.py
@@ -23,7 +23,7 @@ def contact_thing():
 @app.route('/play')
 def fun_thing():
 	return render_template('contact.html')
-@app.route('/googlec19f162caea798b3.html')
+
 def test_thing():
 	return "I am impossible to contact, aside from emails, forums, places I edit etc."
 if __name__ == '__main__':
